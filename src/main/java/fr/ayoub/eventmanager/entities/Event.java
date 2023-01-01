c37a6b9902643ea5391e38e4ee32883330bc257c
vipouchez <eyoubhmema@gmail.com>
2023-01-01 18:58:38 +0100

    'many_to_many'

--- src/main/java/fr/ayoub/eventmanager/entities/Event.java
+++ src/main/java/fr/ayoub/eventmanager/entities/Event.java
@@ -1,8 +1,11 @@
 package fr.ayoub.eventmanager.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -17,8 +20,13 @@ public class Event {
     @ManyToOne
     private Theme theme;
 
-    public String toString(){
+    public String toString() {
         return null;
     }
 
+    @ManyToMany
+    @JoinTable(name ="Event_Sponsor_Associations",
+        joinColumns = @JoinColumn( name ="idEvent"),
+        inverseJoinColumns = @JoinColumn( name = "idSponsor"))
+    private List<Sponsor> Sponsors = new ArrayList<>();
 }
---
+++ src/main/java/fr/ayoub/eventmanager/entities/Sponsor.java
@@ -0,0 +1,24 @@
+package fr.ayoub.eventmanager.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity @Table(name="Event")
+@Data
+public class Sponsor {
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+     private String name;
+     private Double donation;
+     @ManyToMany
+     @JoinTable(name ="Event_Sponsor_Associations",
+         joinColumns = @JoinColumn( name ="idSponsor"),
+         inverseJoinColumns = @JoinColumn( name = "idEvent"))
+     private List<Event> Events = new ArrayList<>();
+
+
+}
